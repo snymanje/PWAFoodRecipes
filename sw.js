2f2ee765732dbd24c3979823a8ebd97b1aca291c
snymanje <jean.snyman@gmail.com>
2020-01-07 15:23:14 +0200

    added fallback

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
 // install service worker
 // self inside the SW refers to itself the SW
 
-const staticCacheName = "site-static-v2";
+const staticCacheName = "site-static-v4";
+const dynamicCacheName = "site-dymanic-v2";
+
 const assets = [
   "/",
   "/index.html",
@@ -12,8 +14,20 @@ const assets = [
   "/css/materialize.min.css",
   "/img/dish.png",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2"
+  "https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
+  "/pages/fallback.html"
 ];
+
+// cahce size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
 self.addEventListener("install", evt => {
   //console.log('Service Worker now installed!', event)
@@ -42,7 +56,7 @@ self.addEventListener("activate", evt => {
       // deleting these keys is an async task so we need to return a promise
       return Promise.all(
         keys
-          .filter(key => key !== staticCacheName)
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
           .map(key => caches.delete(key))
       );
     })
@@ -53,8 +67,23 @@ self.addEventListener("activate", evt => {
 self.addEventListener("fetch", evt => {
   //console.log("fetch event", evt);
   evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
+    caches
+      .match(evt.request)
+      .then(cacheRes => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then(fetchRes => {
+            return caches.open(dynamicCacheName).then(cache => {
+              cache.put(evt.request.url, fetchRes.clone());
+              limitCacheSize(dynamicCacheName, 20);
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.includes(".html"))
+          return caches.match("/pages/fallback.html");
+      })
   );
 });
